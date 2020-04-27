389db1ebc7779713ff406aad4987f6605ab11d1e
jkennedy07git config --global user.namejkennedy07git config --global user.name jkennedy07git config --global user.name jkennedy07 <jlkennedy@gmail.com>
2020-04-26 19:24:40 -0500

    Working on parse rules

--- rpncalc.py
+++ rpncalc.py
@@ -6,14 +6,22 @@
 #
 #
 
+import math
 import ply.lex as lex
 import ply.yacc as yacc
 from pystack import Stack
 
-# required list of token names
+########################
+###### lexer ###########
+########################
+
+# basic parameters
+
+
 tokens = (
     'NUMBER',
     'FLOAT',
+    'EQUALS',
     'PLUS',
     'MINUS',
     'TIMES',
@@ -24,10 +32,11 @@ tokens = (
     'SIN',
     'COS',
     'TAN',
-    'VARIABLE'      # possible addition
+    'NAME'      # possible addition
 )
 
 # regex rules
+t_EQUALS    = r'\='
 t_PLUS      = r'\+'
 t_MINUS     = r'-'
 t_TIMES     = r'\*'
@@ -36,6 +45,7 @@ t_LPAREN    = r'\('
 t_RPAREN    = r'\)'
 t_POWER     = r'\^'     # consider allowing both ^ and ** operators
                         # exponents are not required functionality
+
 # maybe use reserved words for sin cos tan
 # 4/18 seems to be working with this format plus invalid char ignore
 t_SIN       = r'[Ss][Ii][Nn]'
@@ -63,6 +73,7 @@ def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
 # error handling rule for invalid chars
 def t_error(t):
     print(f"Illegal character {t.value[0]}")
@@ -72,7 +83,7 @@ def t_error(t):
 lexer = lex.lex()
 
 # testing data
-data = ''' 35 + 48 33 22 -11 - 33 sin 12 / 55.55 '''
+data = ''' 3 11 +'''
 lexer.input(data)
 
 
@@ -83,34 +94,34 @@ rpnStack = Stack()
 
 # lexer.token() goes through the tokens in order
 # this loop prints everything in the lexer
-while True:
+'''while True:
     tok = lexer.token()
     if not tok:
         break
     print(tok)
 
-### PARSING ###
+'''
 
+#########################
+###### PARSING ##########
+#########################
 
 # postfix might not require precedence rules
-
 
 # parse grammar rules go here
 # t[0] is the result, t[1] is the first argument, etc.
 
-### Below taken from sample calc #############
-##############################################
+names = { }
 
-# Parsing rules
-
+# left means left associative operators
+# right means the opposite, as in unary minus
+# the list runs from low to high priority
 precedence = (
     ('left','PLUS','MINUS'),
     ('left','TIMES','DIVIDE'),
     ('right','UMINUS'),
     )
 
-# dictionary of names
-names = { }
 
 def p_statement_assign(t):
     'statement : NAME EQUALS expression'
@@ -119,6 +130,10 @@ def p_statement_assign(t):
 def p_statement_expr(t):
     'statement : expression'
     print(t[1])
+
+def p_expression_uminus(t):
+    'expression : MINUS expression %prec UMINUS'
+    t[0] = -t[2]
 
 def p_expression_binop(t):
     '''expression : expression PLUS expression
@@ -130,16 +145,14 @@ def p_expression_binop(t):
     elif t[2] == '*': t[0] = t[1] * t[3]
     elif t[2] == '/': t[0] = t[1] / t[3]
 
-def p_expression_uminus(t):
-    'expression : MINUS expression %prec UMINUS'
-    t[0] = -t[2]
 
 def p_expression_group(t):
     'expression : LPAREN expression RPAREN'
     t[0] = t[2]
 
 def p_expression_number(t):
-    'expression : NUMBER'
+    '''expression : NUMBER
+                  | FLOAT'''
     t[0] = t[1]
 
 def p_expression_name(t):
@@ -154,26 +167,13 @@ def p_error(t):
     print("Syntax error at '%s'" % t.value)
 
 
-
-
-
-
-
-
-
-
-##############################################
-
-
 # build parser
-# parser = yacc.yacc()
-
-# RPN evaluation function
+parser = yacc.yacc()
 
 
-
-
-
+# RPN evaluation function?
+# probably not needed as yacc will use stack and recursion
+# internally to evaluate complex inputs
 
 """
 def RPN(stack, tok):
@@ -195,10 +195,13 @@ def RPN(stack, tok):
 
 
 # main function loop
-""" while True:
-    print("Reverse Polish Notation Calculator (type \"help\" for more information)")
+while True:
+    print("Reverse Polish Notation Calculator")
     try:
-        s = input('calc > ')   # Use raw_input on Python 2
+        s = input(' --> ')   # Use raw_input on Python 2
+        #print(data)
+        #s = data
     except EOFError:
         break
-    parser.parse(s) """
+    parser.parse(s)
+    #break
