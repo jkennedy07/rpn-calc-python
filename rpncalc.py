42d7b7547329b6392d1dbc9f74da42044bca3bdb
jkennedy07git config --global user.namejkennedy07git config --global user.name jkennedy07git config --global user.name jkennedy07 <jlkennedy@gmail.com>
2020-04-30 18:31:47 -0500

    Cleaned up comments, removed () and /n handling, added sqrt

--- rpncalc.py
+++ rpncalc.py
@@ -16,7 +16,6 @@
 import math
 import ply.lex as lex
 import ply.yacc as yacc
-from pystack import Stack
 
 ########################
 ###### lexer ###########
@@ -26,34 +25,33 @@ from pystack import Stack
 
 
 tokens = (
-    'NUMBER',
+    'INT',
     'FLOAT',
-    'EQUALS',
     'PLUS',
     'MINUS',
     'TIMES',
     'DIVIDE',
-    'POWER',        # not required
-    'LPAREN',
-    'RPAREN',
+    'POWER', 
+    'SQRT',     
     'SIN',
     'COS',
-    'TAN'      
+    'TAN'     
 )
 
 # regex rules
-t_EQUALS    = r'\='
+t_ignore    = ' \t()\n='     # ignore tabs and spaces
+
 t_PLUS      = r'\+'
 t_MINUS     = r'-'
 t_TIMES     = r'\*'
 t_DIVIDE    = r'/'
-t_LPAREN    = r'\('
-t_RPAREN    = r'\)'
-t_POWER     = r'\^'     # consider allowing both ^ and ** operators
-                        # exponents are not required functionality
+t_POWER     = r'\^' 
 
-# maybe use reserved words for sin cos tan
-# 4/18 seems to be working with this format plus invalid char ignore
+def t_SQRT(t):
+    r'[Ss][Qq][Rr][Tt]'
+    t.value = 'SQRT'
+    return t
+
 def t_SIN(t):
     r'[Ss][Ii][Nn]'
     t.value = 'SIN'
@@ -69,28 +67,16 @@ def t_TAN(t):
     t.value = 'TAN'
     return t
 
-# this is for a normal calculator
-# for RPN white space must be tracked (fixed)
-# as configured now it creates separate tokens when spaces are found
-# between numbers
-t_ignore    = ' \t'     # ignore tabs and spaces
-
 def t_FLOAT(t):
     r'[+-]?[0-9]+\.[0-9]+'
     # r'[+-]?[0-9]+(\.[0-9]+)?'     # this regex makes all nums floats
     t.value = float(t.value)
     return t
     
-def t_NUMBER(t):
+def t_INT(t):
     r'\d+'              # regex for 1 or more digits
     t.value = int(t.value) 
     return t
-
-# detects line breaks to track line numbers
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
 
 # error handling rule for invalid chars
 def t_error(t):
@@ -104,33 +90,18 @@ lexer = lex.lex()
 data = ''' 3 11 + 5 Sin'''
 lexer.input(data)
 
-
-rpnStack = Stack()
-
-
-
-
-# lexer.token() goes through the tokens in order
-# this loop prints everything in the lexer
-
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    print(tok)
-
+# lexer.token() goes through the tokens in order when called
+# this loop prints everything in the lexer to the console for testing
+# while True:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     print(tok)
 
 
 #########################
 ###### PARSING ##########
 #########################
-
-# postfix might not require precedence rules
-
-# parse grammar rules go here
-# t[0] is the result, t[1] is the first argument, etc.
-
-# names = { }
 
 # left means left associative operators
 # right means the opposite, as in unary minus
@@ -141,7 +112,7 @@ precedence = (
     ('right','UMINUS'),
     )
 
-
+# t[0] is the result, t[1] is the first argument, etc.
 def p_statement_expr(t):
     'statement : expression'
     print(t[1])
@@ -154,11 +125,17 @@ def p_expression_binop(t):
     '''expression : expression expression PLUS
                   | expression expression MINUS
                   | expression expression TIMES
-                  | expression expression DIVIDE'''
+                  | expression expression DIVIDE
+                  | expression expression POWER'''
     if t[3] == '+'  : t[0] = t[1] + t[2]
     elif t[3] == '-': t[0] = t[1] - t[2]
     elif t[3] == '*': t[0] = t[1] * t[2]
     elif t[3] == '/': t[0] = t[1] / t[2]
+    elif t[3] == '^': t[0] = t[1] ** t[2]
+
+def p_expression_sqrt(t):
+    '''expression : expression SQRT'''
+    if t[2] == 'SQRT' : t[0] = math.sqrt(t[1])
 
 def p_expression_trig(t):
     '''expression : expression SIN
@@ -168,26 +145,16 @@ def p_expression_trig(t):
     elif t[2] == 'COS' : t[0] = math.cos(t[1])
     elif t[2] == 'TAN' : t[0] = math.tan(t[1])
 
-def p_expression_group(t):
-    'expression : LPAREN expression RPAREN'
-    t[0] = t[2]
-
 def p_expression_number(t):
-    '''expression : NUMBER
+    '''expression : INT
                   | FLOAT'''
     t[0] = t[1]
 
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
 
-
 # build parser
 parser = yacc.yacc()
-
-
-# RPN evaluation function?
-# probably not needed as yacc will use stack and recursion
-# internally to evaluate complex inputs
 
 # main function loop
 while True:
@@ -198,9 +165,9 @@ while True:
             print("This program does calculations in Reverse Polish Notation, also called postfix.")
             print("Operations are binary or unary. This calculator is capable of performing")
             print("add, subtract, multiply, divide, and the basic trig functions sin, cos, and tan.")
-            print("Parentheses are optional, but may be desired for clarity.\n")
+            print("Exponents take the form: x y ^ ---> x^y. For square root use \'sqrt\'")
+            print("The calculator ignores parentheses, but you may use them for clarity.\n")
             continue
     except EOFError:
         break
     parser.parse(s)
-    #break
